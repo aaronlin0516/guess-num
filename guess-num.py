131f04a05aa5e180b53d4d4c2cbdd0af9e5954f1
Aaron Lin <aaronlin0516@hotmail.com>
2019-03-20 17:39:34 +0800

    Add guess Number

--- guess-num.py
+++ guess-num.py
@@ -1,10 +1,12 @@
 import random
-	r = random.randint(1, 100)
+r = random.randint(1, 100)
+x = 0
 while True:
 	num = input('請猜數字(1-100): ')
 	num = int(num)
+	x = x + 1
 	if num == r:
-		print('恭喜猜中了!')
+		print('恭喜猜中了! 您共猜了', x,'次')
 		break
 	elif num < r:
 		print('比答案小喔')
